800591824aec9476d14523611aa7d5ae95589d5e
FriesenTJ <friesen.tjou@gmail.com>
2024-01-19 11:50:38 +0800

    Update

--- apache_superset/config/apache_superset.py
+++ apache_superset/config/apache_superset.py
@@ -1,7 +1,18 @@
 from __future__ import unicode_literals
 from frappe import _
+
 def get_data():
     config = [
-        
+        {
+            "label": _("Apache Example"),
+            "items": [
+                {
+                    "type": "doctype",
+                    "name": "cash purchase",
+                    "label": "Cash Purchase Example",
+                    "onboard": 1,
+                }
+            ]
+        },
     ]
     return config
