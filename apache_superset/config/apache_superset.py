a26136deb0e08725daa6ff81e37a30e740be86d0
FriesenTJ <friesen.tjou@gmail.com>
2024-01-22 14:12:14 +0800

    Update

--- apache_superset/config/apache_superset.py
+++ apache_superset/config/apache_superset.py
@@ -4,23 +4,6 @@ from frappe import _
 def get_data():
     config = [
         {
-            "label": _("Apache Example"),
-            "items": [
-                {
-                    "type": "doctype",
-                    "name": "cash purchase",
-                    "label": "Cash Purchase Example",
-                    "onboard": 1,
-                },
-                {
-                    "type": "doctype",
-                    "name": "item purchase",
-                    "label": "Item Purchase Example",
-                    "onboard": 2,
-                }
-            ]
-        },
-        {
             "label": _("Apache Setup"),
             "items": [
                 {
