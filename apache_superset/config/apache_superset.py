91bc925e38074a005337ab8783be4d435b4eb909
FriesenTJ <friesen.tjou@gmail.com>
2024-02-19 11:11:55 +0800

    Update

--- apache_superset/config/apache_superset.py
+++ apache_superset/config/apache_superset.py
@@ -4,7 +4,7 @@ from frappe import _
 def get_data():
     config = [
         {
-            "label": _("Apache Setup"),
+            "label": _("Apache Dashboard"),
             "items": [
                 {
                     "type": "doctype",
